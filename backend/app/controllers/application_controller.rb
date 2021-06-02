2c26a7d3cc1f4cdbc8df96e9bd2cbae3b5bda693
khoinguyenkc <khoinguyenkc@gmail.com>
2021-06-02 14:35:54 -0700

    added devise and jwt to backend

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,37 +1,66 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :null_session
+    attr_reader :current_user
 
-    #how do other controllers get access to this?
-    
-    def is_logged_in
-        #since these are also marked as helper methods, they are availalble in not just all ctronollers (cuz they inherit from appc ontroler) but also avialble to views
-
-        (session[:user_id] && session[:user_id].present? ) ? true : false
+    protected
+    def authenticate_request!
+      unless user_id_in_token?
+        render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+        return
+      end
+      @current_user = User.find(auth_token[:user_id])
+    rescue JWT::VerificationError, JWT::DecodeError
+      render json: { errors: ['Not Authenticated'] }, status: :unauthorized
     end
-
-    def current_user
-        @user = User.find_by(id: session[:user_id])
-    end
-
-
-    def current_email
-        #this TRY to initialize, not guranteed.. if user enter an email when they buy, then the email is saved. we'll later use this to help them signup/login
-            session[:current_email] = current_user.email if current_user && current_user.email
-            session[:current_email] 
-    end
-
-    def current_or_dummy_user
-        if is_logged_in
-            current_user
-        else
-            User.new(email: "dummy@dummy.com", password: SecureRandom.hex )
+  
+    private
+    def http_token
+        @http_token ||= if request.headers['Authorization'].present?
+          request.headers['Authorization'].split(' ').last
         end
     end
-
-
-
-    def clear_email
-        session.delete :current_email if session[:current_email] 
+  
+    def auth_token
+      @auth_token ||= JsonWebToken.decode(http_token)
     end
+  
+    def user_id_in_token?
+      http_token && auth_token && auth_token[:user_id].to_i
+    end
+
+    
+    # #how do other controllers get access to this?
+    
+    # def is_logged_in
+    #     #since these are also marked as helper methods, they are availalble in not just all ctronollers (cuz they inherit from appc ontroler) but also avialble to views
+
+    #     (session[:user_id] && session[:user_id].present? ) ? true : false
+    # end
+
+    # def current_user
+    #     @user = User.find_by(id: session[:user_id])
+    # end
+
+
+    # def current_email
+    #     #this TRY to initialize, not guranteed.. if user enter an email when they buy, then the email is saved. we'll later use this to help them signup/login
+    #         session[:current_email] = current_user.email if current_user && current_user.email
+    #         session[:current_email] 
+    # end
+
+    # def current_or_dummy_user
+    #     if is_logged_in
+    #         current_user
+    #     else
+    #         User.new(email: "dummy@dummy.com", password: SecureRandom.hex )
+    #     end
+    # end
+
+
+
+    # def clear_email
+    #     session.delete :current_email if session[:current_email] 
+    # end
 
 
 
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,16 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_205801) do
+ActiveRecord::Schema.define(version: 2021_06_02_212252) do
 
   create_table "users", force: :cascade do |t|
     t.string "fullname"
     t.string "avatar"
     t.string "handle"
     t.integer "uid"
-    t.string "password_digest"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
